dd90c3ed12286d315f3305db248fc6d8bdab0ee7
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-05-17 10:01:42 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua_chave_secreta'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'segredo-super-seguro')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///alego_admin.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'output', 'disciplinas')

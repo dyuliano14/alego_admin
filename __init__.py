18152857584759ba0da6940bc8d0aa093710b8e4
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-05-17 09:01:33 -0300

    Delete __init__.py
    exclusao de extruturas

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# Importe as rotas APENAS quando a aplicação estiver inicializada
-# Isso é feito importando o módulo 'routes' no final do arquivo __init__.py
-from . import routes

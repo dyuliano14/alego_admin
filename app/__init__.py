c853ba018c566a21ed021a7026b8ebfbbf189351
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-05-17 08:56:40 -0300

    Create __init__.py
    atualização da estrutura

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    db.init_app(app)
+    csrf.init_app(app)
+
+    with app.app_context():
+        from . import routes
+        db.create_all()
+        return app

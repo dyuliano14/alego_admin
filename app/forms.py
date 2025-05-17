6032facbb9eea27caf34ac26ad85db66f0a06410
Dyuliano Magalhães <dyuliano.magalhaes@academico.ifg.edu.br>
2025-05-17 10:12:37 -0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -5,3 +5,8 @@ from wtforms.validators import DataRequired
 class DisciplinaForm(FlaskForm):
     nome_disciplina = StringField('Nome da Disciplina', validators=[DataRequired()])
     submit = SubmitField('Criar')
+
+class LoginForm(FlaskForm):
+    username = StringField('Usuário', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Entrar')
